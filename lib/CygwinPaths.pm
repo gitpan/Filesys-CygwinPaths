f3496550b1279dc86e0c2232e6a3590de0de62b9
Sören Andersen <somian@cpan.org>
2002-11-14 07:43:07 +0000

    Import of SOMIAN/Filesys-CygwinPaths-0.03 from CPAN.
    gitpan-cpan-distribution: Filesys-CygwinPaths
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SOMIAN/experimental/unregistered/Filesys-CygwinPaths-0.03.tar.gz
    gitpan-cpan-author:       SOMIAN
    gitpan-cpan-maturity:     released

--- lib/CygwinPaths.pm
+++ lib/CygwinPaths.pm
@@ -1,5 +1,9 @@
 package Filesys::CygwinPaths;
 
+# This is EXPERIMENTAL, alpha-type code. It is not yet promised that the API
+# will remain as is. Please send bug reports and feature requests (of a
+# modest nature, please) to the address in the POD below.
+
 use 5.006;
 use strict;
 use warnings;
@@ -17,10 +21,6 @@ require DynaLoader;
 
 our @ISA = qw(Exporter DynaLoader);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
 # This allows declaration	use Filesys::CygwinPaths ':all';
 # If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
 # will save memory.
@@ -28,10 +28,10 @@ our %EXPORT_TAGS = ( 'all' => [ qw( vetted_path
      fullposixpath posixpath fullwin32path win32path )
   ]  );
 
-our @EXPORT = ( qw{ PATHS_protocol vetted_path } );
+our @EXPORT = ( qw{ $PATHSMODE PATHS_mode vetted_path } );
 our @EXPORT_OK = ( qw{fullposixpath posixpath fullwin32path win32path},
-                   '$PATHS_PROTOCOL');
-use vars qw( $VERSION $PATHS_PROTOCOL );
+                   '$PATHSMODE');
+use vars qw( $VERSION $PATHSMODE );
 
 =head1 NAME
 
@@ -46,7 +46,7 @@ my $discard=length <<'=head1 VERSION';
 
 =head1 VERSION
 
-$VERSION = '0.02' ;
+$VERSION = '0.03' ;
 
 =cut
 
@@ -54,15 +54,10 @@ $VERSION = '0.02' ;
 
 bootstrap Filesys::CygwinPaths $VERSION;
 
-sub MAX_PATH {
-  return 260 - 1; # how it is defined in sys/params.h
-}
-
-
 =head1 SYNOPSIS
 
     use Filesys::CygwinPaths;
-    PATHS_protocol('cyg_win32');
+    PATHS_mode('cyg_win32');
 	my $HOME = $ENV{'HOME'};
 
     my @pics_to_ogle = glob("$HOME/mypics/*.jpg");
@@ -78,57 +73,79 @@ sub MAX_PATH {
     use Filesys::CygwinPaths ':all';
 	my $windows_groks_this = fullwin32path("$ENV{HOME}");
 	my $posix_style = fullposixpath($ENV{'USERPROFILE'});
-	if(posixpath($windows_groks_this) ne $posix_style) {
+
+	if(posixpath($windows_groks_this) ne $posix_style)
+	{
 	   print "You don't keep your bash HOME in your NT Profile dir, huh?\n";
     }
 
 =cut
 
+
+
+sub PATHS_mode {
+  my $selfobj ; # OO-ready
+  if(ref $_[0]) {
+	$selfobj = shift;
+  }
+  if(not $_[0] and not defined $PATHSMODE) {
+	$PATHSMODE = 'cyg_mixed';
+  } elsif(defined $_[0]) {
+	  my $arg = shift;
+	  $arg eq 'cyg_mixed'?
+		 do{ $PATHSMODE = $arg; }
+	: $arg eq 'cyg_posix'?
+	     do{ $PATHSMODE = $arg; }
+	: $arg eq 'cyg_win32'?
+	     do{ $PATHSMODE = $arg; }
+	:
+      do{ Carp::croak "Invalid PATHSMODE name: \"$arg\" -- "; }
+	; # end psuedo case / switch statement.
+  }
+  $selfobj->{'PATHSMODE'} = $PATHSMODE if defined($selfobj); 
+  return $PATHSMODE;
+}
+
+
+sub _handle_tilde {
+
+  my $arg = shift;
+  return $arg unless $arg =~/^~/;
+  my $pathsep = (defined($PATHSMODE) && $PATHSMODE eq 'cyg_win32')?
+          '\\' : '/';
+  my @homey = split( /\/|$pathsep/, $arg, 2 );
+  Carp::croak "Cannot understand your arg: $_[0] -- " unless
+     $homey[0] =~/^~\w*\z/;
+  # pattern ' m/^~(\w+)?/ ' is tilde with optional username
+	 $homey[0] =~ s%^~(\w*)%$1 ?
+		   ((getpwnam($1))[7] || "~$1")
+		:   (getpwuid($>))[7]
+		      %ex;
+  return join( $pathsep, $homey[0],$homey[1] );
+}
+
 sub vetted_path {
   my $returnpath;
-  my $inpath = shift;
-  if(not defined $PATHS_PROTOCOL) {
-	Carp::carp 'You ought to set $PATHS_PROTOCOL'.
+  my $inpath = _handle_tilde(shift);
+  if(not defined $PATHSMODE) {
+	Carp::carp 'You ought to set $PATHSMODE'.
 	 ' before calling this subroutine!'.
 	 "\nDefaulting to 'cyg_mixed' style. -- ";
-	 &PATHS_protocol();
+	$PATHSMODE = &PATHS_mode('cyg_mixed');
   }
 
   $returnpath =
-   $PATHS_PROTOCOL eq 'cyg_mixed'?
-      do{ ($returnpath = win32path($inpath)) =~s@\\@/@g; }
-  :$PATHS_PROTOCOL eq 'cyg_posix'?
+   $PATHSMODE eq 'cyg_mixed'?
+      do{ $returnpath = win32path($inpath);
+	      $returnpath =~s@\\@/@g; $returnpath; }
+  :$PATHSMODE eq 'cyg_posix'?
       posixpath($inpath)
-  :$PATHS_PROTOCOL eq 'cyg_win32'?
+  :$PATHSMODE eq 'cyg_win32'?
       win32path($inpath)
   : '' # should never happen.
   ; # end psuedo case / switch statement
 
    $returnpath;
-}
-
-
-sub PATHS_protocol {
-  my $selfobj ; # OO-ready
-  if(ref $_[0]) {
-	$selfobj = shift;
-  }
-  if(not $_[0] and not defined $PATHS_PROTOCOL) {
-	$PATHS_PROTOCOL = q[cyg_mixed];
-  } elsif(defined $_[0]) {
-	  my $arg = shift @_;
-	  $arg eq 'cyg_mixed'?
-		 do{ $PATHS_PROTOCOL = $arg; }
-	: $arg eq 'cyg_posix'?
-	     do{ $PATHS_PROTOCOL = $arg; }
-	: $arg eq 'cyg_win32'?
-	     do{ $PATHS_PROTOCOL = $arg; }
-	:
-      do{ Carp::croak "Invalid PATHS_PROTOCOL name: \"$arg\" -- "; }
-	; # end psuedo case / switch statement.
-  }
-  $selfobj->{'PATHS_PROTOCOL'} = $PATHS_PROTOCOL if defined($selfobj); 
-  return $PATHS_PROTOCOL;	
 }
 
 
@@ -141,7 +158,7 @@ sub PATHS_protocol {
 
 sub fullposixpath {
   my ($input, $retval, $output);
-  $input = shift;
+  $input = _handle_tilde(shift);
   if( $input eq '') {
 	Carp::carp "No path argument supplied! -- ";
 	return '';
@@ -152,18 +169,19 @@ sub fullposixpath {
 
 sub fullwin32path {
   my ($input, $retval, $output);
-  $input = shift;
+  $input = _handle_tilde(shift);
   if( $input eq '') {
 	Carp::carp "No path argument supplied! -- ";
 	return '';
   }
-  $output = cygwin_conv_to_full_win32_path($input);
+  $output = ucfirst
+     cygwin_conv_to_full_win32_path($input);
 }
 
 
 sub posixpath {
   my ($input, $retval, $output);
-  $input = shift;
+  $input = _handle_tilde(shift);
   if( $input eq '') {
 	Carp::carp "No path argument supplied! -- ";
 	return '';
@@ -174,7 +192,7 @@ sub posixpath {
 
 sub win32path {
   my ($input, $retval, $output);
-  $input = shift;
+  $input = _handle_tilde(shift);
   if( $input eq '') {
 	Carp::carp "No path argument supplied! -- ";
 	return '';
@@ -188,58 +206,67 @@ __END__
 =head1 DESCRIPTION
 
 B<Filesys::CygwinPaths> is a B<Cygwin-specific> module created to ease the
-author's occasional pique over the little quirks that come up with using Perl
-on Cygwin. The subroutines it exports allow various kinds of path
+author's occasional pique over the little quirks that come up with using
+Perl on Cygwin, the free POSIX emulation psuedoplatform for Microsoft
+Windows(tm). The subroutines it exports allow various kinds of path
 conversions to be made in a fairly concise, simple, procedural manner. At
 the present time the module does not have an OO interface but one might be
 added in the future. The module can be used according to two diffent
 approaches, which are outlined below.
 
-Note: Hopefully it is evident that the module can be neither built nor used
-on any platform besides Perl built for Cygwin, and there would be no reason
-to want to do so.
+Note: Hopefully it is obvious that the module can be neither built nor used
+on any platform besides Perl for Cygwin, and there would be no reason to
+want to do so.
 
 =head2 Usage Styles
 
 Two slightly different ways of using B<Filesys::CygwinPaths> are available. The
 first, and recommended, way is to tell Perl once and for all (for the
-duration of your script) what I<protocol> you need to use in conversing
+duration of your script) what I<mode> you need to use in conversing
 with, say external non-Cygwin applications. To do this, say something like:
 
-S<<    C<<use Filesys::CygwinPaths;>> >>
-S<<    C<<PATHS_protocol( 'cyg_mixed' );>> >>
-S<<    C<<my $own_name = vetted_path($0);>> >>
-S<<    C<<print "I am $own_name, how are you today?\n";>> >>
+	use Filesys::CygwinPaths;
+	 PATHS_mode( 'cyg_mixed' );
+	 my $own_name = vetted_path($0);
+	 print "I am $own_name, how are you today?\n";
 
-And that will set the I<protocol> to C<cyg_mixed> for the duration of the
-script. The 3 recognized settings for C<PATHS_protocol()> are:
+And that will set the I<mode> to C<cyg_mixed> for the duration of the script
+or until you change it to something else.
 
-S<<       C<<cyg_mixed>> ( like: F<<C:/foobar/sugarplum/fairy.txt>> ) >>
-S<<       C<<cyg_win32>> ( like: F<<C:\foobar\sugarplum\fairy.txt>> ) >>
-S<<       C<<cyg_posix>> ( like: F<</cygdrive/c/sugarplum/fairy.txt>> ) >>
+The 3 recognized settings for C<$PATHSMODE> are:
+
+S<       C<cyg_mixed> (like: F<C:/foobar/sugarplum/fairy.txt>) >
+S<       C<cyg_win32> (like: F<C:\foobar\sugarplum\fairy.txt>) >
+S<       C<cyg_posix> (like: F</cygdrive/c/sugarplum/fairy.txt>) >
 
 Alternatively you might prefer the more specific and elaborate full set of
-subroutines to be made available to you (C<:all>). These can be called by
-name to get the specific translation protocol you desire. Listed below.
+subroutines to be made available to you by name (C<:all>). These can be
+called to get the specific translation mode you desire. Listed below.
+However, at present, using C<vetted_path> is the only means by which to get
+"mixed" mode paths.
 
 =over 4
 
 =item vetted_path($path_in)
 
-make any translations necessary to transform the path argument according to the setting of the global (B<Filesysy::CygwinPaths>) variable C<PATHS_PROTOCOL>. If this variable is not already set when C<vetted_path()> is called, it will set to the default of C<cyg_mixed> and complain a little at you. Set the script-wide protocol you desire for C<vetted_path()> to use by
+make any translations necessary to transform the path argument according to the setting of the global (B<Filesysy::CygwinPaths>) variable C<$PATHSMODE>. If this variable is not already set when C<vetted_path()> is called, it will set to the default of C<cyg_mixed> and complain a little at you. Set the script-wide mode you desire for C<vetted_path()> to use by
 
-	(a) setting PATHS_PROTOCOL directly in your script
+=over 3
 
-(not B<my>, it is a variable in the B<Filesys::CygwinPaths> namespace which
-is imported by default, it must therefore not be a lexical in your script or
-will have no effect).
+(a) setting PATHSMODE directly in your script
 
-    (b) calling the subroutine C<PATHS_protocol> with the desired value.
+   $PATHSMODE = 'cyg_posix';  # not "my" (!!)
 
+(b) calling the subroutine C<PATHS_mode> with the desired value. See below.
 
-=item PATHS_protocol(<style>)
+=back
 
-set or query the current I<protocol> under which C<vetted_path> will
+This function provides the "mixed mode" paths that aren't directly provided
+by any functions in the Cygwin API. That is its main raison d'tre.
+
+=item PATHS_mode(<mode>)
+
+set or query the current I<mode> under which C<vetted_path> will
 return a path spec.
 
 
@@ -267,6 +294,17 @@ return the fully-qualified path spec in Windows style.
 
 =back
 
+=head2 Changes
+
+  0.03 Added private subroutine to provide the HOME directory if the
+       arg began with "~<something>". Added ucfirst() call to
+       fullwin32path(), to force uniformity of results (all drive
+       letters UpperCase). Changed all instances of using the term
+       "protocol" in subs and variables to "mode".  ;-)
+
+  0.02 First upload to CPAN (unregistered).
+
+  0.01 First build.
 
 =head2 Notes on the XS programming (C interface)
 
@@ -287,10 +325,11 @@ Another way to do the call to the xs interface would have been, maybe:
 
 =head1 BUGS
 
-Perl does not know how to interpret the C<~/> shell abbreviation for the
-login user's HOME directory (it is B<bash> or another POSIX-y shell that
-does this); therefore do not use C<~/> in args to any of the functions. Use
-$ENV{HOME} instead.
+We shall see.
+
+The return values (int C type) of the Cygwin functions are being discarded.
+The author confesses his crime. This may change in a future update, but for
+now, is how it is.
 
 
 =head1 SEE ALSO
